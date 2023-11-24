3af10df5f6dea0ba58939d7bfef3c3b117d76c28
Alexsandro <jalexsandro2005@gmail.com>
2023-11-23 22:38:22 -0300

    aula 5 - modificação

--- src/components/itemCount/index.tsx
+++ src/components/itemCount/index.tsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 interface Props {
     stock:number;
     initial:number;
-    onAdd: () => void;
+    onAdd: (value:number) => void;
 }
 
 const ItemCount = ({stock, initial=1, onAdd}:Props) => {
@@ -16,18 +16,24 @@ const ItemCount = ({stock, initial=1, onAdd}:Props) => {
         }
     }
 
+    const handleClick = () => {
+        onAdd(count);
+        setCount(initial);
+    }
+
     return(
         <div className="flex flex-col justify-center items-end gap-1">
-            <div className="flex flex-col items-center">
-                <div className="flex border border-gray-300 rounded-sm">
+            <div className="flex border border-gray-300 rounded-sm">
                 <button className="p-2 flex items-center justify-center" onClick={() => {changeCount(-1)}}>-</button>
-                <div className="border-x border-gray-300 py-2 px-4 flex items-center justify-center bg-indigo-50">{count}</div>
+                <div className="border-x border-gray-300 py-2 px-4 flex items-center justify-center bg-indigo-50">
+                    {count}
+                </div>
                 <button className="p-2 flex items-center justify-center" onClick={() => {changeCount(1)}}>+</button>
             </div>
-            {stock <= 0 && <p className="text-xs text-red-500">Fora de estoque</p>}
-            </div>
         
-        <button className="border border-gray-300 text-white bg-slate-800 py-1 px-2 rounded-md shadow-md hover:bg-slate-600" onClick={onAdd}>Adicioar ao carrinho</button>
+            <button className="border border-gray-300 text-white bg-slate-800 py-1 px-2 rounded-md shadow-md hover:bg-slate-600 disabled:bg-slate-600" disabled={stock === 0} onClick={handleClick}>
+                Adicioar ao Carrinho
+            </button>
         </div>
     );
 }
--- src/components/itemListContainer/index.tsx
+++ src/components/itemListContainer/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { ItemCount } from "../itemCount";
 
 interface Props {
@@ -5,11 +6,21 @@ interface Props {
 }
 
 const ItemListContainer = ({greeting}:Props) => {
+
+    const [stock, setStock] = useState<number>(5);
+
+    const changeStock = (value:number) => {
+        setStock(previous => previous - value);
+    }
+
     return (
         <div className="w-1/6 h-1/2 shadow border border-gray-300 bg-gray-50 rounded-md grid grid-rows-4">
             <div className="flex justify-between border-t border-gray-300 row-start-4 p-2">
-                <p>{greeting}</p>
-                <ItemCount stock={5} initial={1}/>
+                <div>
+                    <p>{greeting}</p>
+                    {stock > 0 ? <p className="text-xs">Estoque: {stock}</p> : <p className="text-xs text-red-500">Fora de Estoque</p>}
+                </div>
+                <ItemCount stock={stock} initial={1} onAdd={changeStock}/>
             </div>
         </div>
     );
